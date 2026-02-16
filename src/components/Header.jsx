cb0aa96862cf5fdcd72aedd0009f0653ba719d74
Ziad Elsayyad <ziad.elsyad16@gmail.com>
2026-02-16 16:39:44 +0200

    Add Header component with logo and title

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="/ChefClaude.png" alt="ChefClaude" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
